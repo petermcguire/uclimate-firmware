e07904fcf811cc1f62adc91a46147ad3d90731aa
Peter McGuire <petermcgu@gmail.com>
2022-01-15 16:45:47 -0800

    initial commit

---
+++ src/main.c
@@ -0,0 +1,450 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_sleep.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include "cJSON.h"
+
+#include "driver/i2c.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      "TELUS2348"
+#define EXAMPLE_ESP_WIFI_PASS      "nvhcqk9jc8"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "WEATHER_MONITOR";
+
+static int s_retry_num = 0;
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
+static int i2c_master_port = I2C_MASTER_NUM;
+
+#define ISL29125_SENSOR_ADDR        0x44
+#define SI7021_SENSOR_ADDR          0x40
+
+static void network_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_STOP) {
+        ESP_LOGI(TAG, "WIFI_EVENT_STA_STOP");
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        // why?
+        uint8_t reason = ((wifi_event_sta_disconnected_t*) event_data)->reason;
+        ESP_LOGI(TAG, "disconnect reason: %u", reason);
+        if (reason == WIFI_REASON_ASSOC_LEAVE) {
+            // disconnected as part of stop
+            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        } else {
+            if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+                esp_wifi_connect();
+                s_retry_num++;
+                ESP_LOGI(TAG, "retry to connect to the AP");
+            } else {
+                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            }
+            ESP_LOGI(TAG,"connect to the AP fail");
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    } else {
+        ESP_LOGI(TAG, "unknown event: event_base %c, event_id %d", *event_base, event_id);
+    }
+}
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                if (output_buffer != NULL) {
+                    free(output_buffer);
+                    output_buffer = NULL;
+                }
+                output_len = 0;
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            break;
+    }
+    return ESP_OK;
+}
+
+void wifi_init_sta(void) {
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &network_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &network_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+void wifi_deinit_sta(void) {
+    ESP_ERROR_CHECK(esp_wifi_disconnect());
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+}
+
+void http_rest_get(void) {
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+
+    esp_http_client_config_t config = {
+        .url = "http://api.gzmo.io",
+        .event_handler = _http_event_handler,
+        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+        .disable_auto_redirect = true,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    // ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+    ESP_LOGI(TAG, "%s", local_response_buffer);
+
+    esp_http_client_cleanup(client);
+}
+
+void http_rest_post(cJSON *payload, uint32_t id) {
+    // char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+    char *post_data;
+    char url[] = "http://api.gzmo.io/sensor/";
+    char _id[32];
+
+    sprintf(_id, "%d", id);
+    strcat(url, _id);
+
+    esp_http_client_config_t config = {
+        .url = url,
+        .event_handler = _http_event_handler,
+        // .user_data = local_response_buffer,        // Pass address of local buffer to get response
+        .disable_auto_redirect = true,
+        .method = HTTP_METHOD_POST,
+        .username = "uclimate",
+        .password = "Y9X]`Dp(/{ycTQuK",
+        .auth_type = HTTP_AUTH_TYPE_BASIC
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    post_data = cJSON_PrintUnformatted(payload);
+
+    // POST
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+    }
+    // // ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+    // ESP_LOGI(TAG, "%s", local_response_buffer);
+
+    esp_http_client_cleanup(client);
+}
+
+esp_err_t i2c_master_init(void) 
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = 18,
+        .scl_io_num = 19,
+        .sda_pullup_en = GPIO_PULLUP_DISABLE,
+        .scl_pullup_en = GPIO_PULLUP_DISABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+esp_err_t sensor_register_read(uint8_t reg_addr, uint8_t *data, size_t len, uint8_t sensor_addr) {
+
+    esp_err_t ret;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, sensor_addr << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_master_port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+    vTaskDelay(30 / portTICK_RATE_MS);
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, sensor_addr << 1 | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (len > 1) {
+        i2c_master_read(cmd, data, len - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, data + len - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_master_port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t isl29125_register_write(uint8_t reg_addr, uint8_t data) {
+
+    esp_err_t ret;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ISL29125_SENSOR_ADDR << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, data, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_master_port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
+
+void app_main() {
+    uint8_t data[2];
+    uint16_t red = 0;
+    uint16_t green = 0;
+    uint16_t blue = 0;
+    uint16_t tmp = 0;
+    double tmp_c = 0.0;
+    uint16_t rh = 0;
+    double rh_p = 0.0;
+    cJSON *root;
+    char *out;
+
+    uint32_t id = 87;
+
+
+    root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "id", cJSON_CreateNumber(id));
+    cJSON_AddItemToObject(root, "red", cJSON_CreateNumber(red));
+    cJSON_AddItemToObject(root, "green", cJSON_CreateNumber(green));
+    cJSON_AddItemToObject(root, "blue", cJSON_CreateNumber(blue));
+    cJSON_AddItemToObject(root, "tmp", cJSON_CreateNumber(tmp_c));
+    cJSON_AddItemToObject(root, "rh", cJSON_CreateNumber(rh_p));
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // ESP_ERROR_CHECK(esp_read_mac(mac, 0));
+
+    // ESP_LOGI(TAG, "mac: %u", *mac);
+
+
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
+    ESP_ERROR_CHECK(isl29125_register_write(0x01, 0x0D));
+    // time for conversion
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // while (true) {
+    // vTaskDelay(3600000 / portTICK_PERIOD_MS);
+    // get RED
+    ESP_ERROR_CHECK(sensor_register_read(0x0C, data, 1, ISL29125_SENSOR_ADDR));
+    red = data[0];
+    ESP_ERROR_CHECK(sensor_register_read(0x0B, data, 1, ISL29125_SENSOR_ADDR));
+    red = (red << 8) + data[0];
+    // ESP_LOGI(TAG, "RED = %u", red);
+    // get GREEN
+    ESP_ERROR_CHECK(sensor_register_read(0x0A, data, 1, ISL29125_SENSOR_ADDR));
+    green = data[0];
+    ESP_ERROR_CHECK(sensor_register_read(0x09, data, 1, ISL29125_SENSOR_ADDR));
+    green = (green << 8) + data[0];
+    // ESP_LOGI(TAG, "GREEN = %u", green);
+    // get BLUE
+    ESP_ERROR_CHECK(sensor_register_read(0x0E, data, 1, ISL29125_SENSOR_ADDR));
+    blue = data[0];
+    ESP_ERROR_CHECK(sensor_register_read(0x0D, data, 1, ISL29125_SENSOR_ADDR));
+    blue = (blue << 8) + data[0];
+    // isl29125 power down
+    ESP_ERROR_CHECK(isl29125_register_write(0x01, 0x00));
+    // ESP_LOGI(TAG, "BLUE = %u", blue);
+    cJSON_ReplaceItemInObjectCaseSensitive (root, "red", cJSON_CreateNumber(red));
+    cJSON_ReplaceItemInObjectCaseSensitive (root, "green", cJSON_CreateNumber(green));
+    cJSON_ReplaceItemInObjectCaseSensitive (root, "blue", cJSON_CreateNumber(blue));
+    // 
+    // temp
+    //
+    ESP_ERROR_CHECK(sensor_register_read(0xE3, data, 2, SI7021_SENSOR_ADDR));
+    tmp = data[0];
+    tmp = (tmp << 8) + data[1];
+    tmp_c = (175.72*tmp)/65536.0 - 46.85;
+    cJSON_ReplaceItemInObjectCaseSensitive (root, "tmp", cJSON_CreateNumber(tmp_c));
+    //
+    // relative humidity
+    //
+    ESP_ERROR_CHECK(sensor_register_read(0xE5, data, 2, SI7021_SENSOR_ADDR));
+    rh = data[0];
+    rh = (rh << 8) + data[1];
+    rh_p = (125*rh)/65536.0 - 6;
+    cJSON_ReplaceItemInObjectCaseSensitive (root, "rh", cJSON_CreateNumber(rh_p));
+    // out = cJSON_Print(root);
+    // ESP_LOGI(TAG, "%s", out);
+    // free(out);
+    wifi_init_sta();
+    http_rest_post(root, id);
+    // }
+    cJSON_Delete(root);
+    wifi_deinit_sta();
+    esp_sleep_enable_timer_wakeup(3600000000);
+    esp_deep_sleep_start();
+}
